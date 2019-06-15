79e261c07060f47d382d77960f6ee504a67a7593
CloudRex <cloudrex@outlook.com>
2019-06-15 17:32:39 -0400

    Add Run delegate methods in LlvmFunction wrapper

--- IonIR/Target/LlvmFunction.cs
+++ IonIR/Target/LlvmFunction.cs
@@ -1,17 +1,34 @@
+using System;
 using LLVMSharp;
 
 namespace Ion.IR.Target
 {
     public class LlvmFunction : LlvmWrapper<LLVMValueRef>, IVerifiable
     {
-        public LlvmFunction(LLVMValueRef source) : base(source)
+        public LlvmModule Parent { get; }
+
+        public LlvmFunction(LlvmModule parent, LLVMValueRef source) : base(source)
         {
             // TODO: Need to verify source is a function.
+            this.Parent = parent;
+        }
+
+        public LLVMGenericValueRef Run(LLVMGenericValueRef[] arguments)
+        {
+            // Delegate to the parent's execution engine.
+            return this.Parent.ExecutionEngine.RunFunction(this, arguments);
+        }
+
+        public LLVMGenericValueRef Run()
+        {
+            // Delegate to the main handler with zero arguments.
+            return this.Run(new LLVMGenericValueRef[] { });
         }
 
         public bool Verify()
         {
-
+            // TODO: Implement.
+            throw new NotImplementedException();
         }
     }
 }

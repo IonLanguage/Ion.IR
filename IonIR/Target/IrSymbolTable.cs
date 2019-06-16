0fb14bcea5ceac7c2f7f6c5d15fb3c84019d9353
CloudRex <cloudrex@outlook.com>
2019-06-15 22:38:19 -0400

    Place SymbolTable under LlvmModule

--- IonIR/Target/IrSymbolTable.cs
+++ IonIR/Target/IrSymbolTable.cs
@@ -1,14 +1,18 @@
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Ion.IR.Target
 {
     public class IrSymbolTable
     {
+        public Dictionary<string, string> Metadata { get; }
+
         protected readonly LlvmModule module;
 
         public IrSymbolTable(LlvmModule module)
         {
             this.module = module;
+            this.Metadata = new Dictionary<string, string>();
         }
 
         public bool ContainsFunction(string identifier)

c710ce3458504b8117f737c5cf44b3da0cd80a37
CloudRex <cloudrex@outlook.com>
2019-07-06 11:32:47 -0400

    Create Struct construct

--- IonIR/Handling/LlvmVisitor.cs
+++ IonIR/Handling/LlvmVisitor.cs
@@ -69,6 +69,40 @@ namespace Ion.IR.Handling
             return node.VisitChildren(this);
         }
 
+        public Construct VisitStruct(Struct node)
+        {
+            // Ensure target struct exists on the symbol table.
+            if (!context.SymbolTable.structs.Contains(this.TargetIdentifier))
+            {
+                throw new Exception($"Reference to undefined struct named '${this.TargetIdentifier}'");
+            }
+
+            // Retrieve the symbol from the symbol table.
+            StructSymbol symbol = context.SymbolTable.structs[this.TargetIdentifier];
+
+            // Retrieve the target struct's LLVM reference value from the symbol.
+            LLVMTypeRef structDef = symbol.Value;
+
+            // Create a value buffer list.
+            List<LLVMValueRef> values = new List<LLVMValueRef>();
+
+            // Populate body properties.
+            foreach (StructProperty property in node.Body)
+            {
+                // Emit and append the value to the buffer list.
+                values.Add(property.Value.Emit(context));
+            }
+
+            // Create the resulting struct assignment value.
+            LlvmValue assignment = LLVM.ConstNamedStruct(structDef, values.ToArray()).Wrap();
+
+            // Append the assignment value onto the stack.
+            this.valueStack.Push(assignment);
+
+            // Return the node.
+            return node;
+        }
+
         public Construct VisitStructDefProperty(StructDefProperty node)
         {
             // Visit the kind.
